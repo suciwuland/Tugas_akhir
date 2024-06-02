82e03aafcec893182c73a162a03e4966b5297bd1
suci wulandari <suciwulandari2422@gmail.com>
2024-06-02 16:59:11 +0700

    revisi(kernel): liner ke rbf

--- apphealth/apphealth/settings.py
+++ apphealth/apphealth/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'djongo',
-    'apphealth'
+    'apphealth',
+    
 ]
 
 MIDDLEWARE = [
--- apphealth/apphealth/svm/svm.py
+++ apphealth/apphealth/svm/svm.py
@@ -36,7 +36,7 @@ def train_svm_model(data):
     X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42, stratify=y)
     
     # Membuat model SVM
-    svm_model = SVC(kernel='linear', random_state=42)
+    svm_model = SVC(kernel='rbf', random_state=42)
     
     # Validasi silang untuk evaluasi model
     scores = cross_val_score(svm_model, X_train, y_train, cv=5)
